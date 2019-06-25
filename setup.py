a4b9aec62b8e57a246b826aa9520f1a4093e86bd
Dumitru Frunza <dumitru.frunza@noviflow.com>
2019-06-25 10:31:07 -0400

    Extension(extra_compile_args=...)

--- setup.py
+++ setup.py
@@ -4,7 +4,10 @@ from Cython.Build import cythonize
 
 setup(
     name = "PyForms",
+
+    #Extension(extra_compile_args=['-fPIC'], extra_link_args=['-nostdlib'])
+
     ext_modules = cythonize([Extension('pyforms', ['pyforms.pyx'], libraries=['forms', 'X11', 'python3.6m'],
-                                       include_dirs=['build/include'], library_dirs=['build/lib'])],
+                                       include_dirs=['/home/dumitru/source/build/include'], library_dirs=['/home/dumitru/source/build/lib'])],
                             compiler_directives={'language_level': '3'}, build_dir='build')
 )
