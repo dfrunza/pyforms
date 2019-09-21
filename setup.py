cb068e9b6ebc2c7ae77538c524e2cc860b342ec4
Dumitru Frunza <dumitru.frunza@gmail.com>
2019-09-21 16:42:22 -0400

    Update include and lib paths in setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     #Extension(extra_compile_args=['-fPIC'], extra_link_args=['-nostdlib'])
 
     ext_modules = cythonize([Extension('pyforms', ['pyforms.pyx'], libraries=['forms', 'X11', 'python3.6m'],
-                                       include_dirs=['/home/dumitru/source/build/include'], library_dirs=['/home/dumitru/source/build/lib'])],
+                                       include_dirs=['runtime/include', 'runtime/include/python3.6m'], library_dirs=['runtime/lib'])],
                             compiler_directives={'language_level': '3'}, build_dir='build')
 )
