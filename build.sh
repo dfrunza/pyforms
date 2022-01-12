ad2c27b8eed1928f700f0e9a5197324ccbc41f0e
Dumitru Frunza <dumitru.frunza@gmail.com>
2022-01-12 10:53:39 -0500

    build.sh : use $PYVER

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 # Compile XForms with :
 #    $ CFLAGS=-fPIC ./configure --prefix=/usr/local --disable-shared --enable-static
 
+PYVER=3.8
 BASE=$PWD
 
 #mkdir -p $BASE/build
@@ -15,7 +16,7 @@ BASE=$PWD
 # Otherwise linking fails with "undefined reference to `stat'" errors.
 
 cython pyforms.pyx -3
-gcc -fPIC -Wall -fwrapv -fno-strict-aliasing -I/usr/include/python3.8 -I./xforms/lib/include -c pyforms.c
-ld -shared -L/usr/lib/python3.8 -L./xforms/lib -o pyforms-py3.8.so pyforms.o -lpython3.8 -lc -lm -lX11 -lforms
+gcc -fPIC -Wall -fwrapv -fno-strict-aliasing -I/usr/include/python$PYVER -I./xforms/lib/include -c pyforms.c
+ld -shared -L/usr/lib/python$PYVER -L./xforms/lib -o pyforms-py$PYVER.so pyforms.o -lpython$PYVER -lc -lm -lX11 -lforms
 
-cp pyforms-py3.8.so ../nf-tools/pyforms.so
+cp pyforms-py$PYVER.so ../nf-tools/pyforms.so
